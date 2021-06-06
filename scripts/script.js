3444db2cd83e7f27860e2c408b64c946541dc40a
Sarah <sjmeng@ucsd.edu>
2021-06-06 10:44:26 -0700

    test

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,56 @@
 
 import { router } from './router.js';
 
+function randomKey(length) {
+  var result = [];
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+  }
+  var rKey = result.join('');
+}
+
+window.onload()= randomKey();
+
+
+var SplitFactory = require('@splitsoftware/splitio').SplitFactory;
+// Instantiate the SDK
+var factory = SplitFactory({ 
+  core: {
+    authorizationKey: 'k5na1f28j0dnibh2e0gl5nrjl1lpulnqtcp1',
+    // the key can be the logged in
+    // user id, or the account id that 
+    // the logged in user belongs to. 
+    // The type of customer (user, account, custom)
+    // is chosen during Split's sign-up process.
+    key: rKey
+  },
+  startup: {
+    readyTimeout: 1.5 // 1.5 sec
+  }
+});
+// And get the client instance you'll use
+var client = factory.client();
+
+client.on(client.Event.SDK_READY, function() {
+  var treatment = client.getTreatment("double-column2");
+  if (treatment == "on") {
+      // insert code here to show on treatment
+      var main = document.querySelector('main');
+      main.className = 'double-column';
+  } else if (treatment == "off") {
+      // insert code here to show off treatment
+      var main = document.querySelector('main');
+      main.className = 'single-column';
+  } else {
+      // insert your control treatment code here
+      console.log("yo");
+  }
+});
+
+
+
 const headerText = document.querySelector('header > h1');
 const settings = document.querySelector('header > img');
 
